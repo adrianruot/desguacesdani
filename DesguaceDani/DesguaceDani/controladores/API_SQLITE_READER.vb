b820505c8db5eabbbdd02b653dda389c803efa8b
Adrian <adrian.rueda@ikasle.egibide.org>
2024-01-10 21:56:36 +0100

    Ultima act de hoy

---
+++ DesguaceDani/DesguaceDani/controladores/API_SQLITE_READER.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SQLite
+
+Module API_SQLITE_READER
+    REM Este modulo que he creado para mi mismo tiene
+    REM como fin el de poder escribir menos veces la cantidad
+    REM de conexiones que debería usar cada vez que me conecto
+    REM a la base de datos
+
+    Private dbName As String = "vb.db"
+
+    Private dbPath As String = Application.StartupPath & "\" & dbName
+
+    Private connString As String = "Data Source=" & dbPath & ";Version=3"
+
+    Private connection As New SQLiteConnection(connString)
+    Private command As New SQLiteCommand("", connection)
+
+    Sub COMPROBAR_CONEXION()
+        If Not connection.State = ConnectionState.Open Then
+            connection.Open()
+        End If
+    End Sub
+
+    Sub COMANDO_CONEXION(textCmd As String)
+        command.Connection = connection
+        command.CommandText = textCmd
+    End Sub
+
+    Function EJECUTAR() As SQLiteDataReader
+        Try
+
+            Return command.ExecuteReader
+
+        Catch ex As Exception
+
+            MsgBox(ex.ToString)
+
+        End Try
+    End Function
+
+    Sub CERRAR()
+        connection.Close()
+    End Sub
+
+End Module
--- DesguaceDani/DesguaceDani/controladores/LoginControlador.vb
+++ DesguaceDani/DesguaceDani/controladores/LoginControlador.vb
@@ -2,36 +2,18 @@
 
 Module LoginControlador
 
-    Private dbName As String = "vb.db"
-
-    Private dbPath As String = Application.StartupPath & "\" & dbName
-
-    Private connString As String = "Data Source=" & dbPath & ";Version=3"
-
-    Private connection As New SQLiteConnection(connString)
-    Private command As New SQLiteCommand("", connection)
-
     Function IniciarSesion(UsuarioNombre As String, UsuarioContrasenia As String) As String
+        API_SQLITE_READER.COMPROBAR_CONEXION()
+        API_SQLITE_READER.COMANDO_CONEXION("SELECT NOMBRE FROM USUARIOS")
 
-
-        If Not connection.State = ConnectionState.Open Then
-            connection.Open()
-        End If
-
-        command.Connection = connection
-        command.CommandText = "SELECT NOMBRE FROM USUARIOS"
-
-        Dim rdr As SQLiteDataReader = command.ExecuteReader
+        Dim rdr As SQLiteDataReader = API_SQLITE_READER.EJECUTAR()
 
         Using rdr
             While (rdr.Read())
                 Return rdr.GetString(0)
             End While
         End Using
-
-        connection.Close()
-
-
+        API_SQLITE_READER.CERRAR()
         Return "Nada"
     End Function
 
